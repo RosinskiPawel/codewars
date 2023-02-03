550a6c3c4fbf2161f962bde4bc02d5fb74b6bf7b
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2023-02-03 20:06:22 +0100

    new js-file

---
+++ 6kyu/characters-in-string.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+  let obj = {}
+  let tabFromString = string.split('')
+  for (let i of tabFromString) {
+    obj[i] ? obj[i]++ : (obj[i] = 1)
+  }
+  return obj
+}
